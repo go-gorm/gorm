0491675ae87abc57b323328e95862bf91dbe6c81
Jinzhu <wosmvp@gmail.com>
2013-10-26 11:59:58 +0800

    Break find code into sql.go file

--- sql.go
+++ sql.go
@@ -1,18 +1,42 @@
 package gorm
 
 import (
+	"reflect"
 	"strings"
 
 	"fmt"
 )
 
-func (s *Orm) explain(value interface{}, operation string) {
+func (s *Orm) explain(value interface{}, operation string) *Orm {
 	s.setModel(value)
 	switch operation {
 	case "Save":
 		s.saveSql(value)
 	case "Delete":
 		s.deleteSql(value)
+	case "Query":
+		s.querySql(value)
+	}
+	return s
+}
+
+func (s *Orm) querySql(out interface{}) {
+	s.Sql = "SELECT * from users limit 1"
+	return
+}
+
+func (s *Orm) query(out interface{}) {
+	rows, err := s.db.Query(s.Sql)
+	s.Error = err
+	for rows.Next() {
+		dest := reflect.ValueOf(out).Elem()
+		fmt.Printf("%+v", dest)
+		columns, _ := rows.Columns()
+		var values []interface{}
+		for _, value := range columns {
+			values = append(values, dest.FieldByName(value).Addr().Interface())
+		}
+		s.Error = rows.Scan(values...)
 	}
 	return
 }
