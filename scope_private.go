b1c2a22646d5815c911b522676fb26f9e9c44525
Jinzhu <wosmvp@gmail.com>
2014-11-12 10:23:51 +0800

    Remove unnecessary ToSnake

--- scope_private.go
+++ scope_private.go
@@ -475,7 +475,7 @@ func (scope *Scope) related(value interface{}, foreignKeys ...string) *Scope {
 
 				// has one
 				if foreignValue, err := scope.FieldValueByName(foreignKey); err == nil {
-					sql := fmt.Sprintf("%v = ?", scope.Quote(ToSnake(toScope.PrimaryKey())))
+					sql := fmt.Sprintf("%v = ?", scope.Quote(toScope.PrimaryKey()))
 					toScope.inlineCondition(sql, foreignValue).callCallbacks(scope.db.parent.callback.queries)
 					return scope
 				}
