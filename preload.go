7f1a4cf3018786ebe56001ba16bfdf2407f93db9
Nikola Kovacs <nikola.kovacs@gmail.com>
2015-09-24 13:34:51 +0200

    Fix panic: reflect.Value.Addr of unaddressable value

--- preload.go
+++ preload.go
@@ -337,15 +337,24 @@ func (scope *Scope) getColumnsAsScope(column string) *Scope {
 			}
 			if column.Kind() == reflect.Slice {
 				for i := 0; i < column.Len(); i++ {
-					columns = reflect.Append(columns, column.Index(i).Addr())
+					elem := column.Index(i)
+					if elem.CanAddr() {
+						columns = reflect.Append(columns, elem.Addr())
+					}
 				}
 			} else {
-				columns = reflect.Append(columns, column.Addr())
+				if column.CanAddr() {
+					columns = reflect.Append(columns, column.Addr())
+				}
 			}
 		}
 		return scope.New(columns.Interface())
 	case reflect.Struct:
-		return scope.New(values.FieldByName(column).Addr().Interface())
+		field := values.FieldByName(column)
+		if !field.CanAddr() {
+			return nil
+		}
+		return scope.New(field.Addr().Interface())
 	}
 	return nil
 }
