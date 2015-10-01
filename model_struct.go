8a2b73723d7c78a358cee3a1e2f20a04c37098e0
Jinzhu <wosmvp@gmail.com>
2015-10-01 08:06:08 +0800

    Better compatibility for complex sql type

--- model_struct.go
+++ model_struct.go
@@ -461,8 +461,8 @@ func parseTagSetting(str string) map[string]string {
 	for _, value := range tags {
 		v := strings.Split(value, ":")
 		k := strings.TrimSpace(strings.ToUpper(v[0]))
-		if len(v) == 2 {
-			setting[k] = v[1]
+		if len(v) >= 2 {
+			setting[k] = strings.Join(v[1:], ":")
 		} else {
 			setting[k] = k
 		}
