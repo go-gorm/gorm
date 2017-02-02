23abd03a95a16de0ef2d559979ead26b2a3cce66
Jinzhu <wosmvp@gmail.com>
2017-02-02 22:29:41 +0800

    Add error if exists after parse query results

--- callback_query.go
+++ callback_query.go
@@ -78,6 +78,10 @@ func queryCallback(scope *Scope) {
 				}
 			}
 
+			if err := rows.Err(); err != nil {
+				scope.Err(err)
+			}
+
 			if scope.db.RowsAffected == 0 && !isSlice {
 				scope.Err(ErrRecordNotFound)
 			}
