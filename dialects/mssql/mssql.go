c3276eb22108692b73ad35ce75809c52e6563daf
Craig Peterson <cpeterson@stackoverflow.com>
2017-02-21 14:23:01 -0700

    fix issue with mssql NEXT option. Fixes #1205

--- dialects/mssql/mssql.go
+++ dialects/mssql/mssql.go
@@ -129,14 +129,18 @@ func (s mssql) CurrentDatabase() (name string) {
 }
 
 func (mssql) LimitAndOffsetSQL(limit, offset interface{}) (sql string) {
-	if limit != nil {
-		if parsedLimit, err := strconv.ParseInt(fmt.Sprint(limit), 0, 0); err == nil && parsedLimit > 0 {
-			sql += fmt.Sprintf(" FETCH NEXT %d ROWS ONLY", parsedLimit)
-		}
-	}
 	if offset != nil {
 		if parsedOffset, err := strconv.ParseInt(fmt.Sprint(offset), 0, 0); err == nil && parsedOffset > 0 {
 			sql += fmt.Sprintf(" OFFSET %d ROWS", parsedOffset)
+		}
+	}
+	if limit != nil {
+		if parsedLimit, err := strconv.ParseInt(fmt.Sprint(limit), 0, 0); err == nil && parsedLimit > 0 {
+			if sql == "" {
+				// add default zero offset
+				sql += " OFFSET 0 ROWS"
+			}
+			sql += fmt.Sprintf(" FETCH NEXT %d ROWS ONLY", parsedLimit)
 		}
 	}
 	return
