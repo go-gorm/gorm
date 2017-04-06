238d1d8de84f1fde924afe3f9b6229a22b87af69
Code <mycalf@me.com>
2017-04-07 03:54:03 +0800

    fix count() bug

--- scope.go
+++ scope.go
@@ -335,7 +335,7 @@ func (scope *Scope) CombinedConditionSql() string {
 		whereSql = strings.TrimSuffix(strings.TrimPrefix(whereSql, "WHERE ("), ")")
 	}
 	return joinSql + whereSql + scope.groupSQL() +
-		scope.havingSQL() + scope.orderSQL() + scope.limitAndOffsetSQL()
+		scope.havingSQL() + scope.orderSQL() + scope.limitAndOffsetSQL() + scope.Search.countEnd
 }
 
 // Raw set raw sql
@@ -942,7 +942,7 @@ func (scope *Scope) count(value interface{}) *Scope {
 	if query, ok := scope.Search.selects["query"]; !ok || !countingQueryRegexp.MatchString(fmt.Sprint(query)) {
 		if len(scope.Search.group) != 0 {
 			scope.Search.Select("count(*) FROM ( SELECT count(*) ")
-			scope.Search.group += " ) AS count"
+			scope.Search.countEnd = " ) AS count"
 		} else {
 			scope.Search.Select("count(*)")
 		}
