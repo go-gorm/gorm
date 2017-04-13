a0aa2b8c1c0bb67e28fa1f40beb2410f737ae9a3
dand4 <dan@d4software.co.uk>
2017-04-13 15:22:00 +0100

    Fixed build error

--- utils.go
+++ utils.go
@@ -137,7 +137,7 @@ func toQueryMarks(primaryValues [][]interface{}) string {
 
 	for _, primaryValue := range primaryValues {
 		var marks []string
-		for range primaryValue {
+		for _ = range primaryValue {
 			marks = append(marks, "?")
 		}
 
