fa2095429b83966857c240a1512cec14c4abd803
Ivan Valkov <iv.v.valkov@gmail.com>
2017-07-03 10:05:01 +0100

    Update scope.go

--- scope.go
+++ scope.go
@@ -449,7 +449,7 @@ func (scope *Scope) callMethod(methodName string, reflectValue reflect.Value) {
 
 var (
 	columnRegexp        = regexp.MustCompile("^[a-zA-Z\\d]+(\\.[a-zA-Z\\d]+)*$") // only match string like `name`, `users.name`
-	isNumberRegexp      = regexp.MustCompile("^\\s*\\d+\\s*$")             // match if string is number
+	isNumberRegexp      = regexp.MustCompile("^\\s*\\d+\\s*$")                   // match if string is number
 	comparisonRegexp    = regexp.MustCompile("(?i) (=|<>|>|<|LIKE|IS|IN) ")
 	countingQueryRegexp = regexp.MustCompile("(?i)^count(.+)$")
 )
