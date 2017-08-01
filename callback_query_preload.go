6f64b8610da6d5214e9197ed3c1bf8ecf8983c89
Kyle Spraggs <spiffyjr@users.noreply.github.com>
2017-08-01 18:05:11 -0500

    Update callback_query_preload.go (#1553)

--- callback_query_preload.go
+++ callback_query_preload.go
@@ -289,7 +289,12 @@ func (scope *Scope) handleManyToManyPreload(field *Field, conditions []interface
 
 	// generate query with join table
 	newScope := scope.New(reflect.New(fieldType).Interface())
-	preloadDB = preloadDB.Table(newScope.TableName()).Model(newScope.Value).Select("*")
+	preloadDB = preloadDB.Table(newScope.TableName()).Model(newScope.Value)
+
+	if len(preloadDB.search.selects) == 0 {
+		preloadDB = preloadDB.Select("*")
+	}
+
 	preloadDB = joinTableHandler.JoinWith(joinTableHandler, preloadDB, scope.Value)
 
 	// preload inline conditions
