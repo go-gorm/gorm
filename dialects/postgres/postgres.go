0a51f6cdc55d1650d9ed3b4c13026cfa9133b01e
Aetheus <adrianhyk@gmail.com>
2017-10-10 21:28:39 +0800

    add JSONB type (#1626)
    * add JSONB type
    
    * add comments to satisfy gofmt

--- dialects/postgres/postgres.go
+++ dialects/postgres/postgres.go
@@ -6,6 +6,9 @@ import (
 
 	_ "github.com/lib/pq"
 	"github.com/lib/pq/hstore"
+	"encoding/json"
+	"errors"
+	"fmt"
 )
 
 type Hstore map[string]*string
@@ -51,4 +54,24 @@ func (h *Hstore) Scan(value interface{}) error {
 	}
 
 	return nil
+}
+
+// Jsonb Postgresql's JSONB data type
+type Jsonb struct {
+	json.RawMessage
+}
+
+// Value get value of Jsonb
+func (j Jsonb) Value() (driver.Value, error) {
+	return j.MarshalJSON()
+}
+
+// Scan scan value into Jsonb
+func (j *Jsonb) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+
+	return json.Unmarshal(bytes, j)
 }
