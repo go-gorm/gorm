293d60cdd2d74450996b2c1c9715ae67f4611889
Jinzhu <wosmvp@gmail.com>
2017-10-10 23:10:30 +0800

    Update Logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,9 +1,17 @@
 package logger
 
-import "os"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 // Interface logger interface
 type Interface interface {
+	SQL(data ...interface{})
+	Info(data ...interface{})
+	Warn(data ...interface{})
+	Error(data ...interface{})
 }
 
 // LogLevel log level
@@ -12,9 +20,12 @@ type LogLevel int
 // DefaultLogLevel default log level
 var DefaultLogLevel LogLevel
 
+// DefaultLogger default logger
+var DefaultLogger = Logger{log.New(os.Stdout, "\r\n", 0)}
+
 const (
 	// Info print SQL, warn messages and errors
-	Info LogLevel = 1 << iota
+	Info LogLevel = iota - 1
 	// Warn print warn messages and errors
 	Warn
 	// Error print errors
@@ -32,4 +43,36 @@ func init() {
 	default:
 		DefaultLogLevel = Error
 	}
+}
+
+// LogWriter log writer interface
+type LogWriter interface {
+	Println(v ...interface{})
+}
+
+// Logger logger
+type Logger struct {
+	LogWriter
+}
+
+// SQL print SQL statements
+func (logger Logger) SQL(data ...interface{}) {
+}
+
+// Info print info
+func (logger Logger) Info(message string, data ...interface{}) {
+	// TODO show file line number
+	logger.Println(fmt.Sprintf("[info] %v", fmt.Sprintf(message, data...)))
+}
+
+// Warn print warn messages
+func (logger Logger) Warn(message string, data ...interface{}) {
+	// TODO show file line number
+	logger.Println(fmt.Sprintf("[warn] %v", fmt.Sprintf(message, data...)))
+}
+
+// Error print error messages
+func (logger Logger) Error(message string, data ...interface{}) {
+	// TODO show file line number
+	logger.Println(fmt.Sprintf("[error] %v", fmt.Sprintf(message, data...)))
 }
