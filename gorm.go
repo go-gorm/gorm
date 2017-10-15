a699c1e5606aaa51dbbcef4ffacc2bd14e39508f
Jinzhu <wosmvp@gmail.com>
2017-10-15 11:16:24 +0800

    Refactor GORM Config

--- gorm.go
+++ gorm.go
@@ -1,16 +1,14 @@
 package gorm
 
-import "github.com/jinzhu/gorm/logger"
+// DB contains information for current db connection
+type DB struct {
+	// Current operation
+	Value  interface{} // Value current operation data
+	db     SQLCommon
+	search *search
+	values map[string]interface{}
 
-// Config GORM config
-type Config struct {
-	// SingularTable use singular table name, by default, GORM will pluralize your struct's name as table name
-	// Refer https://github.com/jinzhu/inflection for inflection rules
-	SingularTable bool
-
-	// BlockGlobalUpdate generates an error on update/delete without where clause, this is to prevent eventual error with empty objects updates/deletions
-	BlockGlobalUpdate bool
-
-	Logger  logger.Interface
-	LogMode logger.LogMode
+	// Result result fields
+	Error        error
+	RowsAffected int64
 }
