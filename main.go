3d28ad47188f97e96c92a8432f783f0ac969342a
Giuseppe <gm42@users.noreply.github.com>
2018-04-03 15:21:07 +0200

    Do not panic if Begin().Error was ignored

--- main.go
+++ main.go
@@ -491,7 +491,8 @@ func (s *DB) Begin() *DB {
 
 // Commit commit a transaction
 func (s *DB) Commit() *DB {
-	if db, ok := s.db.(sqlTx); ok && db != nil {
+	var emptySQLTx *sql.Tx
+	if db, ok := s.db.(sqlTx); ok && db != nil && db != emptySQLTx {
 		s.AddError(db.Commit())
 	} else {
 		s.AddError(ErrInvalidTransaction)
