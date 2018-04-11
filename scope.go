cde9a1a695f0bab21a5d30a6e4b3c35231faab3a
shane F <shane@jobrapp.com>
2018-04-11 11:49:12 -0700

    fix scope.removeForeignKey method

--- scope.go
+++ scope.go
@@ -1215,7 +1215,7 @@ func (scope *Scope) addForeignKey(field string, dest string, onDelete string, on
 }
 
 func (scope *Scope) removeForeignKey(field string, dest string) {
-	keyName := scope.Dialect().BuildKeyName(scope.TableName(), field, dest)
+	keyName := scope.Dialect().BuildKeyName(scope.TableName(), field, dest, "foreign")
 
 	if !scope.Dialect().HasForeignKey(scope.TableName(), keyName) {
 		return
