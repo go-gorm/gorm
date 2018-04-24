7155023d16953796ad0e7880e87918cded5afe79
jetereting <1570967070@qq.com>
2018-04-24 11:10:01 +0800

    add RawMap

--- main.go
+++ main.go
@@ -443,6 +443,41 @@ func (s *DB) Raw(sql string, values ...interface{}) *DB {
 	return s.clone().search.Raw(true).Where(sql, values...).db
 }
 
+// RawMap sql查询返回[]map[string]string类型
+func (s *DB) RawMap(sqlQuery string, sqlValues ...interface{}) (result []map[string]string, err error) {
+	rows, err := s.Raw(sqlQuery, sqlValues...).Rows()
+	if err != nil {
+		return
+	}
+	cols, err := rows.Columns()
+	if err != nil {
+		return
+	}
+	values := make([]sql.RawBytes, len(cols))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return
+		}
+		var value string
+		resultC := map[string]string{}
+		for i, col := range values {
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			resultC[cols[i]] = value
+		}
+		result = append(result, resultC)
+	}
+	return
+}
+
 // Exec execute raw sql
 func (s *DB) Exec(sql string, values ...interface{}) *DB {
 	scope := s.NewScope(nil)
