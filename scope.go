9627224878aff557b8f149bfaaaf3821c6f61150
lokanadham <lokanadham.m@voonik.com>
2018-07-19 12:15:57 +0530

    Adding Trace attribute Scope to store tracing info

--- scope.go
+++ scope.go
@@ -24,6 +24,7 @@ type Scope struct {
 	skipLeft        bool
 	fields          *[]*Field
 	selectAttrs     *[]string
+	Trace           interface{}
 }
 
 // IndirectValue return scope's reflect value's indirect value
