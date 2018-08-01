b6ab7738599c5ade7aee17a8765fd2117d18bc50
Eason Lin <easonlin404@gmail.com>
2018-08-01 16:01:07 +0800

    fix goimports

--- main_test.go
+++ main_test.go
@@ -12,6 +12,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/erikstmartin/go-testdb"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mssql"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
