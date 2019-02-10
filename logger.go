906799fef2f895116d915e1793314ab9053b400d
Jinzhu <wosmvp@gmail.com>
2019-02-10 15:39:40 +0800

    Better log output for uint*

--- logger.go
+++ logger.go
@@ -64,9 +64,8 @@ var LogFormatter = func(values ...interface{}) (messages []interface{}) {
 						}
 					} else {
 						switch value.(type) {
-						case int8, int, int16, int32, int64, float32, float64, bool:
+						case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, bool:
 							formattedValues = append(formattedValues, fmt.Sprintf("%v", value))
-							break
 						default:
 							formattedValues = append(formattedValues, fmt.Sprintf("'%v'", value))
 						}
