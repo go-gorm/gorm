1a99c7bd1ee78b0576fc0bd022dcbd34051422e0
Michał Poreda <michal.poreda94@gmail.com>
2019-03-14 09:32:29 +0100

    Create model_snake.go
    It would be nice to be able force all models to use snake case on son response. In my projects all api endpoints returns always snake case. Gorm models are the only that are "hard" to do so.

---
+++ model_snake.go
@@ -0,0 +1,15 @@
+package gorm
+
+import "time"
+
+// Model base model definition, including fields `ID`, `CreatedAt`, `UpdatedAt`, `DeletedAt`, which could be embedded in your models
+//    type User struct {
+//      gorm.ModelSC
+//    }
+
+type ModelSC struct {
+	ID        uint       `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `sql:"index" json:"deleted_at"`
+}
