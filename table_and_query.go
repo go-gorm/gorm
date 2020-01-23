b621d4e5ea51e78b7812aa307fd67fe3ec98cec3
Raul-Mircea <raul-mircea@vivino.com>
2020-01-23 14:20:09 +0100

    Fix

--- table_and_query.go
+++ table_and_query.go
@@ -2,7 +2,6 @@ package gorm
 
 import (
 	"strings"
-	"time"
 )
 
 // TableAndQuery returns the table name and the query already formatted as a string
@@ -10,8 +9,9 @@ func (scope *Scope) TableAndQuery() (string, string) {
 	scope.InstanceSet("skip_bindvar", true)
 	scope.prepareQuerySQL()
 
-	qs := LogFormatter("sql", "q", time.Duration(1), scope.SQL, scope.SQLVars, int64(1))
-	t, q := scope.TableName(), qs[3].(string)
+	//qs := LogFormatter("sql", "q", time.Duration(1), scope.SQL, scope.SQLVars, int64(1))
+	//t, q := scope.TableName(), qs[3].(string)
+	t, q := scope.TableName(), scope.SQL
 	if t == "" {
 		qsplit := strings.Fields(strings.ToLower(q))
 		for i := range qsplit {
