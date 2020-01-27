349424e53305bfdc7632756f58c56331d8498e3f
Raul-Mircea <raul-mircea@vivino.com>
2020-01-27 14:53:46 +0100

    Fix

--- table_and_query.go
+++ table_and_query.go
@@ -6,8 +6,7 @@ import (
 )
 
 // TableAndQuery returns the table name and the query already formatted as a string
-func (db *DB) TableAndQuery() (string, string) {
-	scope := db.NewScope(db)
+func (scope *Scope) TableAndQuery() (string, string) {
 	scope.InstanceSet("skip_bindvar", true)
 	//qs := LogFormatter("sql", "q", time.Duration(1), scope.SQL, scope.SQLVars, int64(1))
 	//t, q := scope.TableName(), qs[3].(string)
