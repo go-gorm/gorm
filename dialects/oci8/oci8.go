7ee4ea18a84dc3e632bf3b310c658eb88668b3a0
Jim Lambert <jimlambrt@Jims-MacBook-Pro.local>
2020-02-18 08:02:38 -0500

    updated for codacy requirements

--- dialects/oci8/oci8.go
+++ dialects/oci8/oci8.go
@@ -1,14 +1,16 @@
-// oci8 implements a gorm dialect for oracle
+// package oci8 implements a gorm dialect for oracle
 package oci8
 
 import (
 	"github.com/jinzhu/gorm"
+	// go-oci8 imported for the driver
 	_ "github.com/mattn/go-oci8"
 )
 
 const dialectName = "oci8"
 
 var _ gorm.Dialect = (*oci8)(nil)
+var _ gorm.OraDialect = (*oci8)(nil)
 
 type oci8 struct {
 	db gorm.SQLCommon
@@ -20,6 +22,7 @@ func init() {
 	gorm.RegisterDialect(dialectName, &oci8{})
 }
 
+// GetName returns the dialect name
 func (oci8) GetName() string {
 	return dialectName
 }
