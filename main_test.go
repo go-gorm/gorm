30433f95d838b839f8428a97c6a3e4b9742af830
Kexian Zhong <lingceng2008@gmail.com>
2020-03-05 11:55:49 +0800

    Fix TestTransactionWithBlock panic case
    Panic will rollback case should use "transaction-3".

--- main_test.go
+++ main_test.go
@@ -535,7 +535,7 @@ func TestTransactionWithBlock(t *testing.T) {
 		})
 	})
 
-	if err := DB.First(&User{}, "name = ?", "transcation").Error; err == nil {
+	if err := DB.First(&User{}, "name = ?", "transcation-3").Error; err == nil {
 		t.Errorf("Should not find record after panic rollback")
 	}
 }
