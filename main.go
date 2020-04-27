251529813aa5bb3c18840517e332c5a5663d17bb
rainesli <rainesli@tencent.com>
2020-04-27 09:00:49 +0800

    update

--- main.go
+++ main.go
@@ -620,7 +620,7 @@ func (s *DB) NewRecord(value interface{}) bool {
 // RecordNotFound check if returning ErrRecordNotFound error
 func (s *DB) RecordNotFound() bool {
 	for _, err := range s.GetErrors() {
-		if err == ErrRecordNotFound {
+		if err.Error() == ErrRecordNotFound.Error() {
 			return true
 		}
 	}
