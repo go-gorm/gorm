1b28c187c0374e3a1347221fece12d8d8d5e40c0
Jinzhu <wosmvp@gmail.com>
2020-06-25 08:00:10 +0800

    Fix create with default value

--- migrator/migrator.go
+++ migrator/migrator.go
@@ -65,10 +65,10 @@ func (m Migrator) FullDataTypeOf(field *schema.Field) (expr clause.Expr) {
 	}
 
 	if field.HasDefaultValue && field.DefaultValue != "" {
-		if field.DataType == schema.String && field.DefaultValueInterface != nil {
-			defaultStmt := &gorm.Statement{Vars: []interface{}{field.DefaultValue}}
-			m.Dialector.BindVarTo(defaultStmt, defaultStmt, field.DefaultValue)
-			expr.SQL += " DEFAULT " + m.Dialector.Explain(defaultStmt.SQL.String(), field.DefaultValue)
+		if field.DefaultValueInterface != nil {
+			defaultStmt := &gorm.Statement{Vars: []interface{}{field.DefaultValueInterface}}
+			m.Dialector.BindVarTo(defaultStmt, defaultStmt, field.DefaultValueInterface)
+			expr.SQL += " DEFAULT " + m.Dialector.Explain(defaultStmt.SQL.String(), field.DefaultValueInterface)
 		} else {
 			expr.SQL += " DEFAULT " + field.DefaultValue
 		}
