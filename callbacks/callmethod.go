929c0c576cd55e935cf204a4ee3c492734a4293b
Jinzhu <wosmvp@gmail.com>
2020-06-30 22:47:21 +0800

    Test Hooks For Slice

--- callbacks/callmethod.go
+++ callbacks/callmethod.go
@@ -11,8 +11,10 @@ func callMethod(db *gorm.DB, fc func(value interface{}, tx *gorm.DB) bool) {
 	if called := fc(db.Statement.Dest, tx); !called {
 		switch db.Statement.ReflectValue.Kind() {
 		case reflect.Slice, reflect.Array:
+			db.Statement.CurDestIndex = 0
 			for i := 0; i < db.Statement.ReflectValue.Len(); i++ {
-				fc(db.Statement.ReflectValue.Index(i).Addr().Interface(), tx)
+				fc(reflect.Indirect(db.Statement.ReflectValue.Index(i)).Addr().Interface(), tx)
+				db.Statement.CurDestIndex++
 			}
 		case reflect.Struct:
 			fc(db.Statement.ReflectValue.Addr().Interface(), tx)
