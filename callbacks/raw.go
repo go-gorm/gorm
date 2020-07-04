89ea62077d4f6a1b9de92fd26b7acd6e72eb1761
Jinzhu <wosmvp@gmail.com>
2020-07-04 08:33:10 +0800

    DryRun for RowQuery, Exec, close #3106

--- callbacks/raw.go
+++ callbacks/raw.go
@@ -5,7 +5,7 @@ import (
 )
 
 func RawExec(db *gorm.DB) {
-	if db.Error == nil {
+	if db.Error == nil && !db.DryRun {
 		result, err := db.Statement.ConnPool.ExecContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
 		if err != nil {
 			db.AddError(err)
--- callbacks/row.go
+++ callbacks/row.go
@@ -10,10 +10,12 @@ func RowQuery(db *gorm.DB) {
 			BuildQuerySQL(db)
 		}
 
-		if _, ok := db.Get("rows"); ok {
-			db.Statement.Dest, db.Error = db.Statement.ConnPool.QueryContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
-		} else {
-			db.Statement.Dest = db.Statement.ConnPool.QueryRowContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
+		if !db.DryRun {
+			if _, ok := db.Get("rows"); ok {
+				db.Statement.Dest, db.Error = db.Statement.ConnPool.QueryContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
+			} else {
+				db.Statement.Dest = db.Statement.ConnPool.QueryRowContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
+			}
 		}
 	}
 }
