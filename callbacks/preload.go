e3b4e0418f2c9c4670bf21f6d9d63caa5a0903ce
Jinzhu <wosmvp@gmail.com>
2020-11-20 15:11:02 +0800

    Inherit SkipHooks option when preloading associations, close #3772

--- callbacks/preload.go
+++ callbacks/preload.go
@@ -13,7 +13,7 @@ func preload(db *gorm.DB, rels []*schema.Relationship, conds []interface{}) {
 	var (
 		reflectValue     = db.Statement.ReflectValue
 		rel              = rels[len(rels)-1]
-		tx               = db.Session(&gorm.Session{NewDB: true})
+		tx               = db.Session(&gorm.Session{NewDB: true, SkipHooks: db.Statement.SkipHooks})
 		relForeignKeys   []string
 		relForeignFields []*schema.Field
 		foreignFields    []*schema.Field
