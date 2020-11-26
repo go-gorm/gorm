590e73ff95d8af6bd14f0a0da687dd7d12e5f94e
rokeyzhao <rokeyzhao@tencent.com>
2020-11-26 20:27:55 +0800

    test: no cache preload in goroutine

--- tests/preload_test.go
+++ tests/preload_test.go
@@ -5,7 +5,10 @@ import (
 	"regexp"
 	"sort"
 	"strconv"
+	"sync"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
@@ -211,4 +214,47 @@ func TestPreloadEmptyData(t *testing.T) {
 	} else if !regexp.MustCompile(`"Team":\[\],"Languages":\[\],"Friends":\[\]`).MatchString(string(r)) {
 		t.Errorf("json marshal is not empty slice, got %v", string(r))
 	}
+}
+
+// TestPreloadGoroutine goroutine中在没有缓存的情况下使用Preload
+func TestPreloadGoroutine(t *testing.T) {
+	// 这里不要用Create，因为会缓存relations
+	// 把tests_test.go中init()函数中的RunMigrations()注释掉，数据库中保留一些数据来触发Preload
+	var wg sync.WaitGroup
+
+	DB = DB.Where("id = ?", 1)
+	tx := DB.Session(&gorm.Session{})
+
+	wg.Add(2)
+	for i := 0; i < 2; i++ {
+		go func() {
+			defer wg.Done()
+			var user2 []User
+			tx = tx.Preload("Team")
+			err := tx.Find(&user2).Error
+
+			ast := assert.New(t)
+			// 这里会报错,Team: unsupported relations
+			// relations是空的
+			ast.Equal(err, nil)
+		}()
+	}
+	wg.Wait()
+}
+
+// TestPreloadNotGoroutine 正常情况在没有缓存的时候下使用Preload
+func TestPreloadNotGoroutine(t *testing.T) {
+	// 这里不要用Create，因为会缓存relations
+	// 把tests_test.go中init()函数中的RunMigrations()注释掉，数据库中保留一些数据来触发Preload
+	DB = DB.Where("id = ?", 1)
+	tx := DB.Session(&gorm.Session{})
+	
+	var user2 []User
+	tx = tx.Preload("Team")
+	err := tx.Find(&user2).Error
+
+	ast := assert.New(t)
+	// 这里不会报错
+	// relations不是空的
+	ast.Equal(err, nil)
 }
