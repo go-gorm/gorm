435bf7086589a69361f5063348ec38768149d071
Jinzhu <wosmvp@gmail.com>
2021-01-05 21:31:51 +0800

    Add OnConflict OnConstraint support, close #3882

--- clause/on_conflict.go
+++ clause/on_conflict.go
@@ -1,11 +1,12 @@
 package clause
 
 type OnConflict struct {
-	Columns   []Column
-	Where     Where
-	DoNothing bool
-	DoUpdates Set
-	UpdateAll bool
+	Columns      []Column
+	Where        Where
+	OnConstraint string
+	DoNothing    bool
+	DoUpdates    Set
+	UpdateAll    bool
 }
 
 func (OnConflict) Name() string {
@@ -28,6 +29,12 @@ func (onConflict OnConflict) Build(builder Builder) {
 	if len(onConflict.Where.Exprs) > 0 {
 		builder.WriteString("WHERE ")
 		onConflict.Where.Build(builder)
+		builder.WriteByte(' ')
+	}
+
+	if onConflict.OnConstraint != "" {
+		builder.WriteString("ON CONSTRAINT ")
+		builder.WriteString(onConflict.OnConstraint)
 		builder.WriteByte(' ')
 	}
 
