ba5bfca7339ad5fbd613ae63c104d132b31c8e17
juancer <cervantesj@gmail.com>
2021-01-23 05:45:22 +0100

    Update model.go
    improvement in gorm Model

--- model.go
+++ model.go
@@ -8,8 +8,12 @@ import "time"
 //      gorm.Model
 //    }
 type Model struct {
-	ID        uint `gorm:"primarykey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt DeletedAt `gorm:"index"`
+	ID        int64      `gorm:"primarykey" json:"id"`
+	CreatedBy int64      `gorm:"created_by" json:"created_by,omitempty"`
+	UpdatedBy int64      `gorm:"updated_by" json:"updated_by,omitempty"`
+	DeletedBy int64      `gorm:"deleted_by" json:"deleted_by,omitempty"`
+	Deleted   bool       `gorm:"deleted" json:"deleted"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+	DeletedAt *time.Time `gorm:"index" json:"deleted_at,omitempty"`
 }
