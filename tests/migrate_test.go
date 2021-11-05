8efabf38d1c0187dec16b0b04edd6831ce25ddcd
Mayank Govilla <mgovilla@gmail.com>
2021-11-04 20:15:03 -0400

    create tests for self-ref has one migration

--- tests/migrate_test.go
+++ tests/migrate_test.go
@@ -54,6 +54,27 @@ func TestMigrate(t *testing.T) {
 	}
 }
 
+func TestAutoMigrateSelfReferential(t *testing.T) {
+	type MigratePerson struct {
+		ID        uint
+		Name      string
+		ManagerID *uint
+		Manager   *MigratePerson
+	}
+
+	if err := DB.Debug().Migrator().DropTable(&MigratePerson{}); err != nil {
+		t.Fatalf("Failed to drop table, got error %v", err)
+	}
+
+	if err := DB.AutoMigrate(&MigratePerson{}); err != nil {
+		t.Fatalf("Failed to auto migrate, but got error %v", err)
+	}
+
+	if !DB.Migrator().HasConstraint("migrate_people", "fk_migrate_people_manager") {
+		t.Fatalf("Failed to find has one constraint between people and managers")
+	}
+}
+
 func TestSmartMigrateColumn(t *testing.T) {
 	fullSupported := map[string]bool{"mysql": true, "postgres": true}[DB.Dialector.Name()]
 
