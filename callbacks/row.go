9d5f315b6d5382dfbdaa20d46751e894b577d337
heige <zhuwei313@hotmail.com>
2021-11-29 09:33:20 +0800

    feat: go code style adjust and optimize code for callbacks package (#4861)
    * feat: go code style adjust and optimize code for callbacks package
    
    * Update scan.go

--- callbacks/row.go
+++ callbacks/row.go
@@ -7,16 +7,17 @@ import (
 func RowQuery(db *gorm.DB) {
 	if db.Error == nil {
 		BuildQuerySQL(db)
-
-		if !db.DryRun {
-			if isRows, ok := db.Get("rows"); ok && isRows.(bool) {
-				db.Statement.Settings.Delete("rows")
-				db.Statement.Dest, db.Error = db.Statement.ConnPool.QueryContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
-			} else {
-				db.Statement.Dest = db.Statement.ConnPool.QueryRowContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
-			}
-
-			db.RowsAffected = -1
+		if db.DryRun {
+			return
 		}
+
+		if isRows, ok := db.Get("rows"); ok && isRows.(bool) {
+			db.Statement.Settings.Delete("rows")
+			db.Statement.Dest, db.Error = db.Statement.ConnPool.QueryContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
+		} else {
+			db.Statement.Dest = db.Statement.ConnPool.QueryRowContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
+		}
+
+		db.RowsAffected = -1
 	}
 }
