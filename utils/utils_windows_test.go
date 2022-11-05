cf71c4fb6cb37a57a00dd436e779782e71ba2dcd
kvii <13721952202@163.com>
2022-11-05 12:56:06 +0800

    fix bug in windows

--- utils/utils_windows_test.go
+++ utils/utils_windows_test.go
@@ -1,3 +1,6 @@
+//go:build windows
+// +build windows
+
 package utils
 
 import "testing"
@@ -8,20 +11,20 @@ func TestSourceDir(t *testing.T) {
 		want string
 	}{
 		{
-			file: `C:\Users\name\go\pkg\mod\gorm.io\gorm@v1.20.8\utils\utils.go`,
-			want: `C:\Users\name\go\pkg\mod\gorm.io`,
+			file: `C:/Users/name/go/pkg/mod/gorm.io/gorm@v1.2.3/utils/utils.go`,
+			want: `C:/Users/name/go/pkg/mod/gorm.io/`,
 		},
 		{
-			file: `C:\go\work\proj\gorm\utils\utils.go`,
-			want: `C:\go\work\proj\gorm`,
+			file: `C:/go/work/proj/gorm/utils/utils.go`,
+			want: `C:/go/work/proj/gorm/`,
 		},
 		{
-			file: `C:\go\work\proj\gorm_alias\utils\utils.go`,
-			want: `C:\go\work\proj\gorm_alias`,
+			file: `C:/go/work/proj/gorm_alias/utils/utils.go`,
+			want: `C:/go/work/proj/gorm_alias/`,
 		},
 		{
-			file: `C:\go\work\proj\my.gorm.io\gorm\utils\utils.go`,
-			want: `C:\go\work\proj\my.gorm.io\gorm`,
+			file: `C:/go/work/proj/my.gorm.io/gorm@v1.2.3/utils/utils.go`,
+			want: `C:/go/work/proj/my.gorm.io/gorm@v1.2.3/`,
 		},
 	}
 	for _, c := range cases {
