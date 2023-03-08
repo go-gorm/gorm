7e84b6b4b55cb1f21bbbe4d64357a63643d6e019
Saeid Saeidee <s.saeidee@sensysgatso.com>
2023-03-08 21:30:18 +0100

    refactor: translate error only when it is not nil

--- gorm.go
+++ gorm.go
@@ -347,13 +347,13 @@ func (db *DB) Callback() *callbacks {
 
 // AddError add error to db
 func (db *DB) AddError(err error) error {
-	if errTranslator, ok := db.Dialector.(ErrorTranslator); ok {
-		err = errTranslator.Translate(err)
-	}
-
 	if db.Error == nil {
 		db.Error = err
 	} else if err != nil {
+		if errTranslator, ok := db.Dialector.(ErrorTranslator); ok {
+			err = errTranslator.Translate(err)
+		}
+
 		db.Error = fmt.Errorf("%v; %w", db.Error, err)
 	}
 	return db.Error
