80259bb77c142354a323bc3afc92e1c3274b2515
Saeid Saeidee <s.saeidee@sensysgatso.com>
2023-03-23 20:19:53 +0100

    refactor: translatorError flag added for backward compatibility

--- tests/error_translator_test.go
+++ tests/error_translator_test.go
@@ -9,10 +9,20 @@ import (
 )
 
 func TestDialectorWithErrorTranslatorSupport(t *testing.T) {
+	// it shouldn't translate error when the TranslateError flag is false
 	translatedErr := errors.New("translated error")
+	untranslatedErr := errors.New("some random error")
 	db, _ := gorm.Open(tests.DummyDialector{TranslatedErr: translatedErr})
 
-	err := db.AddError(errors.New("some random error"))
+	err := db.AddError(untranslatedErr)
+	if errors.Is(err, translatedErr) {
+		t.Fatalf("expected err: %v got err: %v", translatedErr, err)
+	}
+
+	// it should translate error when the TranslateError flag is true
+	db, _ = gorm.Open(tests.DummyDialector{TranslatedErr: translatedErr}, &gorm.Config{TranslateError: true})
+
+	err = db.AddError(untranslatedErr)
 	if !errors.Is(err, translatedErr) {
 		t.Fatalf("expected err: %v got err: %v", translatedErr, err)
 	}
