e550ed71b6d7b0bca8499e579ed2842103a1e475
Saeid Saeidee <s.saeidee@sensysgatso.com>
2023-06-11 21:17:41 +0200

    test: coverage for foreign key violation err

--- tests/error_translator_test.go
+++ tests/error_translator_test.go
@@ -58,3 +58,48 @@ func TestSupportedDialectorWithErrDuplicatedKey(t *testing.T) {
 		t.Fatalf("expected err: %v got err: %v", gorm.ErrDuplicatedKey, err)
 	}
 }
+
+func TestSupportedDialectorWithErrForeignKeyViolated(t *testing.T) {
+	type Country struct {
+		gorm.Model
+		Name string `gorm:"unique"`
+	}
+
+	type City struct {
+		gorm.Model
+		Name      string `gorm:"unique"`
+		CountryID uint
+		Country   Country `gorm:"Constraint:OnUpdate:CASCADE,OnDelete:CASCADE;FOREIGNKEY:CountryID;References:ID"`
+	}
+
+	db, err := OpenTestConnection(&gorm.Config{TranslateError: true})
+	if err != nil {
+		t.Fatalf("failed to connect database, got error %v", err)
+	}
+
+	dialectors := map[string]bool{"sqlite": true, "postgres": true, "mysql": false, "sqlserver": false}
+	if supported, found := dialectors[db.Dialector.Name()]; !(found && supported) {
+		return
+	}
+
+	if err = db.AutoMigrate(&Country{}, &City{}); err != nil {
+		t.Fatalf("failed to migrate countries & cities tables, got error: %v", err)
+	}
+
+	country := &Country{Name: "Netherlands"}
+
+	err = db.Create(country).Error
+	if err != nil {
+		t.Fatalf("failed to create country: %v", err)
+	}
+
+	err = db.Create(&City{Name: "Amsterdam", CountryID: country.ID}).Error
+	if err != nil {
+		t.Fatalf("failed to create city: %v", err)
+	}
+
+	err = db.Create(&City{Name: "Rotterdam", CountryID: 123}).Error
+	if !errors.Is(err, gorm.ErrForeignKeyViolated) {
+		t.Fatalf("expected err: %v got err: %v", gorm.ErrForeignKeyViolated, err)
+	}
+}
