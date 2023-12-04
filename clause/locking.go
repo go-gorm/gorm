c95e45f45e3c02e590d898f7bf20466ec4a3874d
dogenkigen <mlaskowsk@gmail.com>
2023-12-04 13:36:45 +0100

    Making locking parameters more intuitive

--- clause/locking.go
+++ clause/locking.go
@@ -1,9 +1,23 @@
 package clause
 
+type LockingStrength string
+
+const (
+	LockingStrengthUpdate = LockingStrength("UPDATE")
+	LockingStrengthShare  = LockingStrength("SHARE")
+)
+
+type LockingOptions string
+
+const (
+	LockingOptionsSkipLocked = LockingOptions("SKIP LOCKED")
+	LockingOptionsNoWait     = LockingOptions("NOWAIT")
+)
+
 type Locking struct {
-	Strength string
+	Strength LockingStrength
 	Table    Table
-	Options  string
+	Options  LockingOptions
 }
 
 // Name where clause name
@@ -13,7 +27,7 @@ func (locking Locking) Name() string {
 
 // Build build where clause
 func (locking Locking) Build(builder Builder) {
-	builder.WriteString(locking.Strength)
+	builder.WriteString(string(locking.Strength))
 	if locking.Table.Name != "" {
 		builder.WriteString(" OF ")
 		builder.WriteQuoted(locking.Table)
@@ -21,7 +35,7 @@ func (locking Locking) Build(builder Builder) {
 
 	if locking.Options != "" {
 		builder.WriteByte(' ')
-		builder.WriteString(locking.Options)
+		builder.WriteString(string(locking.Options))
 	}
 }
 
