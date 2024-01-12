0123dd45094295fade41e13550cd305eb5e3a848
Jacky <jacky-943572677@qq.com>
2024-01-12 18:09:22 +0900

    fix: ignore .gen.go suffix in logger to get the real caller when using gen #6697 (#6785)

--- utils/utils.go
+++ utils/utils.go
@@ -35,7 +35,8 @@ func FileWithLineNum() string {
 	// the second caller usually from gorm internal, so set i start from 2
 	for i := 2; i < 15; i++ {
 		_, file, line, ok := runtime.Caller(i)
-		if ok && (!strings.HasPrefix(file, gormSourceDir) || strings.HasSuffix(file, "_test.go")) {
+		if ok && (!strings.HasPrefix(file, gormSourceDir) || strings.HasSuffix(file, "_test.go")) &&
+			!strings.HasSuffix(file, ".gen.go") {
 			return file + ":" + strconv.FormatInt(int64(line), 10)
 		}
 	}
