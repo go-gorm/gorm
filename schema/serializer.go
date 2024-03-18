e0c3be03fba966a0128f4d215fa3743d0e8e9a6e
Jinzhu <wosmvp@gmail.com>
2024-03-18 16:28:46 +0800

    Fix tests in local

--- schema/serializer.go
+++ schema/serializer.go
@@ -126,12 +126,12 @@ func (UnixSecondSerializer) Value(ctx context.Context, field *Field, dst reflect
 	rv := reflect.ValueOf(fieldValue)
 	switch v := fieldValue.(type) {
 	case int64, int, uint, uint64, int32, uint32, int16, uint16:
-		result = time.Unix(reflect.Indirect(rv).Int(), 0)
+		result = time.Unix(reflect.Indirect(rv).Int(), 0).UTC()
 	case *int64, *int, *uint, *uint64, *int32, *uint32, *int16, *uint16:
 		if rv.IsZero() {
 			return nil, nil
 		}
-		result = time.Unix(reflect.Indirect(rv).Int(), 0)
+		result = time.Unix(reflect.Indirect(rv).Int(), 0).UTC()
 	default:
 		err = fmt.Errorf("invalid field type %#v for UnixSecondSerializer, only int, uint supported", v)
 	}
