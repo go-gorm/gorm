8abaddf4b4bf9539f23989eb39af8592b321c34d
snackmgmg <16898622+snackmgmg@users.noreply.github.com>
2024-03-19 22:09:09 +0900

    fix: remove callback from callbacks if Remove() called

--- callbacks.go
+++ callbacks.go
@@ -186,12 +186,23 @@ func (p *processor) Replace(name string, fn func(*DB)) error {
 }
 
 func (p *processor) compile() (err error) {
-	var callbacks []*callback
+	var (
+		callbacks []*callback
+		removed   []string
+	)
 	for _, callback := range p.callbacks {
 		if callback.match == nil || callback.match(p.db) {
 			callbacks = append(callbacks, callback)
 		}
+		if callback.remove {
+			removed = append(removed, callback.name)
+		}
 	}
+
+	if len(removed) > 0 {
+		callbacks = removeCallbacks(callbacks, removed)
+	}
+
 	p.callbacks = callbacks
 
 	if p.fns, err = sortCallbacks(p.callbacks); err != nil {
@@ -338,4 +349,15 @@ func sortCallbacks(cs []*callback) (fns []func(*DB), err error) {
 	}
 
 	return
+}
+
+func removeCallbacks(cs []*callback, names []string) []*callback {
+	callbacks := make([]*callback, 0, len(cs))
+	for _, callback := range cs {
+		if utils.Contains(names, callback.name) {
+			continue
+		}
+		callbacks = append(callbacks, callback)
+	}
+	return callbacks
 }
