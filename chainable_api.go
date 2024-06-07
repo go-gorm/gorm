81af77620e5832a10a6e53ba0e56df9e01cf6d5f
supergem3000 <zhengyuechen@live.cn>
2024-06-07 16:56:41 +0800

    feat: chainable order support clause.OrderBy

--- chainable_api.go
+++ chainable_api.go
@@ -299,10 +299,16 @@ func (db *DB) Having(query interface{}, args ...interface{}) (tx *DB) {
 //
 //	db.Order("name DESC")
 //	db.Order(clause.OrderByColumn{Column: clause.Column{Name: "name"}, Desc: true})
+//  db.Order(clause.OrderBy{Columns: []clause.OrderByColumn{
+//      {Column: clause.Column{Name: "name"}, Desc: true},
+//      {Column: clause.Column{Name: "age"}, Desc: true},
+//  }})
 func (db *DB) Order(value interface{}) (tx *DB) {
 	tx = db.getInstance()
 
 	switch v := value.(type) {
+	case clause.OrderBy:
+		tx.Statement.AddClause(v)
 	case clause.OrderByColumn:
 		tx.Statement.AddClause(clause.OrderBy{
 			Columns: []clause.OrderByColumn{v},
