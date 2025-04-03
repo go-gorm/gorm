78fb8e2a6a77cc2b0acc6597fe15ffe3ae77a09a
Radmehr Soleimanian <131693036+hertzcodes@users.noreply.github.com>
2025-04-03 10:12:16 +0330

    Update logger.go

--- logger/logger.go
+++ logger/logger.go
@@ -164,14 +164,14 @@ func (l *logger) Trace(ctx context.Context, begin time.Time, fc func() (string, 
 
 	elapsed := time.Since(begin)
 	switch {
-	case err != nil && l.LogLevel >= Error && (!errors.Is(err, ErrRecordNotFound) || !l.IgnoreRecordNotFoundError):
+	case err != nil && l.LogLevel <= Error && (!errors.Is(err, ErrRecordNotFound) || !l.IgnoreRecordNotFoundError):
 		sql, rows := fc()
 		if rows == -1 {
 			l.Printf(l.traceErrStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, "-", sql)
 		} else {
 			l.Printf(l.traceErrStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, rows, sql)
 		}
-	case elapsed > l.SlowThreshold && l.SlowThreshold != 0 && l.LogLevel >= Warn:
+	case elapsed > l.SlowThreshold && l.SlowThreshold != 0 && l.LogLevel <= Warn:
 		sql, rows := fc()
 		slowLog := fmt.Sprintf("SLOW SQL >= %v", l.SlowThreshold)
 		if rows == -1 {
@@ -179,7 +179,7 @@ func (l *logger) Trace(ctx context.Context, begin time.Time, fc func() (string, 
 		} else {
 			l.Printf(l.traceWarnStr, utils.FileWithLineNum(), slowLog, float64(elapsed.Nanoseconds())/1e6, rows, sql)
 		}
-	case l.LogLevel == Info:
+	case l.LogLevel <= Info:
 		sql, rows := fc()
 		if rows == -1 {
 			l.Printf(l.traceStr, utils.FileWithLineNum(), float64(elapsed.Nanoseconds())/1e6, "-", sql)
