a57abbe12651e3d7a25ddae48ec8b37792053a0d
Chris Moran <chris@chrismoran.com>
2025-10-31 08:13:56 -0400

    Add Namer-based column lookup to Schema.LookUpField (#7619)
    * feat: add namer-driven column name lookup in schema field retrieval
    
    * fix: safely check for underlying type == nil
    
    * fix: reverted "safe" check
    
    * chore: PR feedback

--- schema/schema.go
+++ schema/schema.go
@@ -82,6 +82,16 @@ func (schema *Schema) LookUpField(name string) *Field {
 	if field, ok := schema.FieldsByName[name]; ok {
 		return field
 	}
+
+	// Lookup field using namer-driven ColumnName
+	if schema.namer == nil {
+		return nil
+	}
+	namerColumnName := schema.namer.ColumnName(schema.Table, name)
+	if field, ok := schema.FieldsByDBName[namerColumnName]; ok {
+		return field
+	}
+
 	return nil
 }
 
