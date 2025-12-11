fbbefe293b48de6566d7b1d09306e43f12188229
slinDocumatrix <151838226+slinDocumatrix@users.noreply.github.com>
2025-12-11 05:44:08 -0300

    Remove compatibility message

--- dialect.go
+++ dialect.go
@@ -74,7 +74,6 @@ func newDialect(name string, db SQLCommon) Dialect {
 		return dialect
 	}
 
-	fmt.Printf("`%v` is not officially supported, running under compatibility mode.\n", name)
 	commontDialect := &commonDialect{}
 	commontDialect.SetDB(db)
 	return commontDialect
